b530e285135df7ef4ffc610bd1cf7fa67ed569c0
Alim-Oxcytech <alim.mohd@oxcytech.com>
2023-11-21 18:53:30 +0530

    work on portfolio

--- src/routes/PortfolioRouter.js
+++ src/routes/PortfolioRouter.js
@@ -1,6 +1,8 @@
 require("dotenv").config();
 const express = require("express");
 const pool = require("../database/connection");
+const { default: isEmail } = require("validator/lib/isemail");
+const { dateConversion } = require("../utils/common-functions");
 const router = express.Router();
 
 //getAll_Accepted_PortTM;
@@ -25,12 +27,13 @@ router.get(
   "/portfolio/get-portfolio-accepted-notification/:pim_id",
   async (req, res) => {
     const { pim_id } = req.params;
+    const { reg_id } = req.body;
     try {
       const [rows] = await pool.execute(
-        "CALL get_portfolio_accepted_notification(?)",
-        [pim_id]
+        "CALL get_portfolio_accepted_notification(?,?)",
+        [pim_id, reg_id]
       );
-      return res.status(200).json({ rows });
+      return res.status(200).json(rows[0]);
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: "Internal server error." });
@@ -43,9 +46,12 @@ router.patch("/portfolio/update-member/:pim_id", async (req, res) => {
   const { pim_id } = req.params;
   try {
     if (pim_id) {
-      const id = `reg_id  = '${pim_id}'`;
+      const id = `pim_id  = '${pim_id}'`;
+      const updateData = Object.entries(req.body)
+        .map(([key, value]) => `${key} = "${value}"`)
+        .join(", ");
       await pool.execute("CALL UpdateProjectPortfolioMember(?, ?)", [
-        req.body,
+        updateData,
         id,
       ]);
       res.status(201).json({ message: "Member updated successfully." });
@@ -62,17 +68,13 @@ router.get("/portfolio/check-member-active/:port_id", async (req, res) => {
   const { port_id } = req.params;
   const { sent_to } = req.body;
   try {
-    if (port_id) {
-      const id = `reg_id  = '${port_id}'`;
-      await pool.execute("CALL check_PortfolioMemberActive(?, ?)", [
-        id,
-        sent_to,
-      ]);
-      res.status(201).json({ message: "Member activated successfully." });
-    } else {
-      return res.status(400).json({ error: "User not found." });
-    }
+    const [rows] = await pool.execute(
+      "CALL check_PortfolioMemberActive(?, ?)",
+      [sent_to, port_id]
+    );
+    return res.status(200).json(rows[0]);
   } catch (err) {
+    console.log(err);
     res.status(500).json({ error: "Internal server error." });
   }
 });
@@ -226,7 +228,7 @@ router.get("/portfolio/get-country-by-code/:country_code", async (req, res) => {
     const [rows] = await pool.execute("CALL getCountryByCode(?)", [
       country_code,
     ]);
-     return res.status(200).json(rows[0]);
+    return res.status(200).json(rows[0]);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "Internal server error." });
@@ -242,12 +244,156 @@ router.get(
       const [rows] = await pool.execute("CALL get_PortfolioDepartment(?)", [
         portfolio_id,
       ]);
-           return res.status(200).json(rows[0]);
+      return res.status(200).json(rows[0]);
     } catch (err) {
       console.error(err);
       res.status(500).json({ error: "Internal server error." });
     }
   }
 );
+
+//InsertPortfolio
+router.post("/portfolio/insert-portfolio", async (req, res) => {
+  try {
+    let { email_address } = req.body;
+
+    if (!isEmail(email_address)) {
+      return res.status(400).json({ error: "Invalid email address." });
+    }
+    const formattedDate = dateConversion();
+    const additionalFields = {
+      portfolio_createddate: formattedDate,
+    };
+
+    const requestBodyWithAdditionalFields = {
+      ...req.body,
+      ...additionalFields,
+    };
+    const paramNamesString = Object.keys(requestBodyWithAdditionalFields).join(
+      ", "
+    );
+    const paramValuesString = Object.values(requestBodyWithAdditionalFields)
+      .map((value) => `'${value}'`)
+      .join(", ");
+
+    const callProcedureSQL = `CALL InsertPortfolio(?, ?)`;
+    await pool.execute(callProcedureSQL, [paramNamesString, paramValuesString]);
+
+    res.status(201).json({
+      message: "Portfolio created successfully.",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+//UpdateProjectPortfolio
+router.patch("/portfolio/update-portfolio/:portfolio_id", async (req, res) => {
+  const { portfolio_id } = req.params;
+  try {
+    const updateData = Object.entries(req.body)
+      .map(([key, value]) => `${key} = "${value}"`)
+      .join(", ");
+    const id = `portfolio_id  = '${portfolio_id}'`;
+    await pool.execute("CALL UpdateProjectPortfolio(?, ?)", [updateData, id]);
+    res.status(201).json({ message: "Portfolio updated successfully." });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+//InsertProjectPortfolioDepartment;
+router.post("/portfolio/insert-project-department", async (req, res) => {
+  try {
+    const formattedDate = dateConversion();
+    const additionalFields = {
+      createddate: formattedDate,
+    };
+
+    const requestBodyWithAdditionalFields = {
+      ...req.body,
+      ...additionalFields,
+    };
+    const paramNamesString = Object.keys(requestBodyWithAdditionalFields).join(
+      ", "
+    );
+    const paramValuesString = Object.values(requestBodyWithAdditionalFields)
+      .map((value) => `'${value}'`)
+      .join(", ");
+
+    const callProcedureSQL = `CALL InsertProjectPortfolioDepartment(?, ?)`;
+    await pool.execute(callProcedureSQL, [paramNamesString, paramValuesString]);
+
+    res.status(201).json({
+      message: "Department added successfully.",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+//UpdateProjectPortfolioDepartment
+router.patch("/portfolio/update-project-department/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateData = Object.entries(req.body)
+      .map(([key, value]) => `${key} = "${value}"`)
+      .join(", ");
+    const portfolio_dept_id = `portfolio_dept_id  = '${id}'`;
+    await pool.execute("CALL UpdateProjectPortfolioDepartment(?, ?)", [
+      updateData,
+      portfolio_dept_id,
+    ]);
+    res.status(201).json({ message: "Department updated successfully." });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
+
+//InsertProjectPortfolioMember;
+router.post("/portfolio/insert-project-member", async (req, res) => {
+  try {
+    const paramNamesString = Object.keys(req.body).join(", ");
+    const paramValuesString = Object.values(req.body)
+      .map((value) => `'${value}'`)
+      .join(", ");
+
+    const callProcedureSQL = `CALL InsertProjectPortfolioMember(?, ?)`;
+    await pool.execute(callProcedureSQL, [paramNamesString, paramValuesString]);
+
+    res.status(201).json({
+      message: "Project member added successfully.",
+    });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "Internal Server Error", details: error.message });
+  }
+});
+
+//UpdateProjectPortfolioMember
+router.patch("/portfolio/update-project-member/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const updateData = Object.entries(req.body)
+      .map(([key, value]) => `${key} = "${value}"`)
+      .join(", ");
+    const pim_id = `pim_id  = '${id}'`;
+    await pool.execute("CALL UpdateProjectPortfolioMember(?, ?)", [
+      updateData,
+      pim_id,
+    ]);
+    res.status(201).json({ message: "Project member updated successfully." });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Internal server error." });
+  }
+});
 
 module.exports = router;
