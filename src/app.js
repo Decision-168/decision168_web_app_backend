2254ad91cf0271edbf7974c44eecc38ce57c95c5
Alim-Oxcytech <alim.mohd@oxcytech.com>
2023-12-29 17:51:51 +0530

    changes in app

--- src/app.js
+++ src/app.js
@@ -32,7 +32,21 @@ app.use(
     optionsSuccessStatus: 204,
   })
 );
-
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://decesion168-s3-cicd.s3-website-us-east-1.amazonaws.com"
+  );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(cors());
 app.use(User);
 app.use(Dashboard);
