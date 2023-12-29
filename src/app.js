8e0d3c42b6705949e75c8ab1567ee24eaf1e0966
Alim-Oxcytech <alim.mohd@oxcytech.com>
2023-12-29 15:21:27 +0530

    edit app.js file

--- src/app.js
+++ src/app.js
@@ -14,11 +14,8 @@ const Tasks = require("./routes/TasksRouter");
 const Project = require("./routes/ProjectRouter");
 const UpgradePlan = require("./routes/UpgradePlanRouter");
 const PORT = process.env.PORT || 3000;
-// console.log(process.env.PORT);
 
 require("./database/connection");
-
-app.use("/d168-app-webhooks", express.raw({ type: "*/*" }));
 app.use(express.json({ limit: "10mb" }));
 app.use(bodyParser.json({ limit: "500mb" }));
 app.use(bodyParser.urlencoded({ limit: "500mb", extended: true }));
@@ -33,18 +30,5 @@ app.use(Goal);
 app.use(Tasks);
 app.use(Project);
 app.use(UpgradePlan);
-
-// Static API endpoint for /api/users
-app.get("/api/users", (req, res) => {
-  // Static user data (replace this with your actual data)
-  const users = [
-    { id: 1, name: "John Doe", email: "john.doe@example.com" },
-    { id: 2, name: "Jane Doe", email: "jane.doe@example.com" },
-    // Add more users as needed
-  ];
-
-  // Send the static user data as JSON
-  res.json(users);
-});
 
 app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
